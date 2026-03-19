9eca74daec008a5539a0b9a69caaa95c22dc48a2
meera <meeramohan323@gmail.com>
2026-03-19 16:01:19 +0000

    Fixed-ci/cd

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,7 @@
 def test_login(page):
     page.goto("https://www.saucedemo.com/")
     page.fill("#user-name", "standard_user")
-    page.fill("#password", "secret_sauco")
+    page.fill("#password", "secret_sauce")
     page.click("#login-button")
 
     assert "inventory" in page.url
